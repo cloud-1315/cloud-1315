4f2b9e33cd363ac38252430b37c6bea129fe365b
ml5w3s <marcelo.lauriano@gmail.com>
2022-11-04 15:05:33 -0300

    Implementação de sessões no processo de login

--- controle/logar_usuario.php
+++ controle/logar_usuario.php
@@ -1,6 +1,6 @@
 <?php
 include("conexao.php");
-
+session_start();
 $user = $_POST["txt_user"];
 $pass = md5($_POST["txt_pass"]);
 
@@ -11,9 +11,14 @@ $row = $result->fetch(PDO::FETCH_ASSOC);
 $usuario = $row[usuario];
 $senha = $row[senha];
 
-if($usuario == $user and $pass==md5($senha)) {
-	$_SESSION['usuario'] = $usuario;
+if($usuario == $user and $pass==md5($senha)) {		
+
+	$_SESSION['usuario'] = $user;
+	$_SESSION['senha'] = $pass;
+	header('location:../index.html');
 } else {
-	header('Location: ../formulario/form_login.php');
+	unset ($_session['$user'];
+	unset ($_session['$pass'];	
+	echo "Usuario ou senha incorretos, <a href='../formulario/form_login.php'>tente novamente</a>";
 }
 ?>
