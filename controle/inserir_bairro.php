62283c50dc652367c4eb996a3b4e57ff4abf0a1f
ml5w3s <marcelo.lauriano@gmail.com>
2022-11-23 01:16:37 -0300

    modelo no cad bairro

--- controle/inserir_bairro.php
+++ controle/inserir_bairro.php
@@ -9,9 +9,18 @@
 <?php
 include("conexao.php");
 $conn = conectar();
+try{
 $bairro = $_POST["txt_bairro"];
-$conn->query("INSERT INTO bairro(nome_bairro) VALUES ('$bairro')");
+if(isset($bairro)){
+	$stm = $conn->prepare("INSERT INTO bairro(nome_bairro) VALUES (?)");
+	$stm->bindValue(1,$bairro,PDO::PARAM_STR);
+	$stm->execute();
+}
+echo "<script language='javascript' type='text/javascript'>window.setInterval(alert('Cadastrado com sucesso',3000);</script>";
 header('Location: http://localhost/escola_a30');
+}catch(PDOException $ex_){
+	echo 'Erro '. $ex->getMessage();
+}
 ?>
 </head>
 </html>
--- formulario/cad_bairro.php
+++ formulario/cad_bairro.php
@@ -5,16 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Escola</title>
-	<link rel="stylesheet" type="text/css" href="estilo/geral.css">    
+	<link rel="stylesheet" type="text/css" href="../estilo/geral.css">    
 </head>
 <body>
+<div id="flex-container">
+<article class="boxinterno">
 <h3>Cadastro de bairro</h3>
     <form method="post" action="../controle/inserir_bairro.php">
-    <fieldset>        
+    <fieldset><br>        
         <label>Digite o bairro</label>
         <input type="text" name="txt_bairro"><br>
-        <input type="submit" value="Cadastrar" class="botoes">
-        </fieldset>
+        <input type="submit" value="Cadastrar" class="botoes"><br><br>
+    </fieldset>
     </form>
+</article>
+</div>
 </body>
 </html>
